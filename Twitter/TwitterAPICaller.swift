c105d51991e24b8b29f81fdd1cc078f4791fab94
Julio Valdes <juliovaldes@Julios-MBP.lan>
2022-10-01 00:22:46 -0500

    Commit before update

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "yTQMnHXaGZyrT5pChzy7WkQ53", consumerSecret: "I1qrJoaJfADXF8DVBLQpD9sB3XU2lpzfDWOiCPwsDDFYkgRbp6")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
